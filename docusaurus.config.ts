c8cf5b80e6f3cf4d56a667682b1e1b7be574e701
Eric_Lian <git@superexercisebook.com>
2025-05-14 20:06:01 +0800

    chore: update base url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://morizerodev.github.io',
+  url: 'https://milease.milthm.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '',
